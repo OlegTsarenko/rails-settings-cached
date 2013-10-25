b3eaebb4342b0cc0ec11e1f0ed3970927bbe7e79
Oleg Tsarenko <o.tsarenko@111min.com>
2013-10-25 10:32:43 +0300

    Add a dependency from environment

--- lib/rails-settings/cached_settings.rb
+++ lib/rails-settings/cached_settings.rb
@@ -1,21 +1,25 @@
 module RailsSettings
 	class CachedSettings < Settings
-    after_update :rewrite_cache    
+    after_update :rewrite_cache
     after_create :rewrite_cache
+
 		def rewrite_cache
-			Rails.cache.write("settings:#{self.var}", self.value)
-		end
-    
-   after_destroy { |record| Rails.cache.delete("settings:#{record.var}") }
-    
+			Rails.cache.write("settings_#{ self.class.env}:#{self.var}", self.value)
+    end
+
+    def self.env
+      Rails.methods.include?(:env) ? Rails.env : 'test'
+    end
+
+    after_destroy { |record| Rails.cache.delete("settings_#{self.class.env}:#{record.var}") }
 
 		def self.[](var_name)
-			obj = Rails.cache.fetch("settings:#{var_name}") {
+			obj = Rails.cache.fetch("settings_#{env}:#{var_name}") {
 				super(var_name)
 			}
       obj || @@defaults[var_name.to_s]
-    end    
-    
+    end
+
     def self.save_default(key,value)
       if self.send(key) == nil
         self.send("#{key}=",value)
